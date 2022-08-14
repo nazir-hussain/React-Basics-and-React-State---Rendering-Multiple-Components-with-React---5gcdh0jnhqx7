5e36c66fa6c60ff81ca7aaead0cef097575c385e
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2022-08-14 17:04:04 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,13 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
-  const pname = "project-name";
-  const pdesc = "project-description";
   return (
     <div id="main">
-      <h1 data-ns-test={pname}>Memories App</h1>
-
-      <p data-ns-test={pdesc}>
-        An App to store my favourite memories of my life
-      </p>
+    <h1 data-ns-test="project-name">Dream Project</h1>
+    <p data-ns-test="project-description">Project Description</p>
     </div>
   );
-};
+}
 
 
 export default App;
