23b616a32d1c4fa723e27e3f9e366729b742ddfd
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2022-08-23 14:53:24 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -37,17 +37,42 @@
 
 
 
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+// import React, {Component, useState} from "react";
+// import '../styles/App.css';
+
+// const App = () => {
+//   return (
+//     <div id="main">
+//     <h1 data-ns-test="project-name">Dream Project</h1>
+//     <p data-ns-test="project-description">Project Description</p>
+//     </div>
+//   );
+// }
+
+
+// export default App;
+
+
+
+
+
+
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main">
-    <h1 data-ns-test="project-name">Dream Project</h1>
-    <p data-ns-test="project-description">Project Description</p>
-    </div>
+    <>
+      <h1 data-ns-test="project-name">EcoLife</h1>
+      <p data-ns-test="project-description">
+        A Ecommerce website for Environment friendly products
+      </p>
+      <h1 data-ns-test="project-name">InterviewPrep</h1>
+      <p data-ns-test="project-description">
+        A website which contains all the blogs related to web developement
+      </p>
+    </>
   );
-}
-
+};
 
 export default App;
